29e17a3332e629d2a752b4ad99810a84fb576f4e
szarlej <jakub.szarleja@gmail.com>
2023-02-20 23:54:55 +0100

    Added bump effect on cart after adding new item

--- src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,16 +1,37 @@
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../CartIcon/CartIcon";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CartContext } from "../../../CartContext/CartContext";
 
 const HeaderCartButton = () => {
+	const [isBtnHighlighted, setIsBtnHighlighted] = useState(false);
 	const ctx = useContext(CartContext);
 	const { items } = ctx;
+
+	useEffect(() => {
+		if (items.length === 0) return;
+
+		const id = setTimeout(() => {
+			setIsBtnHighlighted(false);
+		}, 300);
+
+		setIsBtnHighlighted(true);
+
+		return () => {
+			clearTimeout(id);
+		};
+	}, [items]);
+
+	const btnClasses = `${styles.button} ${
+		isBtnHighlighted ? styles.bump : ""
+	}`;
+
 	const itemsAmount = items.reduce((acc, item) => {
 		return acc + item.amount;
 	}, 0);
+
 	return (
-		<button onClick={ctx.switchVisibility} className={styles.button}>
+		<button onClick={ctx.switchVisibility} className={btnClasses}>
 			<span className={styles.icon}>
 				<CartIcon></CartIcon>
 			</span>
